ea467e5e2ffd64c669578575a47a49d9d8ed83a1
Heytan69 <92723545+Heytan69@users.noreply.github.com>
2021-11-11 14:23:32 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalkan</h1>";
+        echo "nama saya febi tania
+            saya tingal di daerah cilebut
+            olahraga yang saya sukai adalah menari";
+    }
+}
---
+++ view-latihan1.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>latihan1</title>
+</head>
+
+<body>
+    halo kawan.. yuk belajar web programming!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode pemjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
