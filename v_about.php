db3cd4164483170d4cc726db0c0d6f15cd763dfa
Heytan69 <92723545+Heytan69@users.noreply.github.com>
2021-11-16 19:35:42 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : febi</li>
+ <li>Nama Belakang : tania</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> kp batugede cilebut barat sukaraja bogor</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>bogor</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>menari</li>
+ <li>bernyanyi</li>
+ </ul>
+</section>
